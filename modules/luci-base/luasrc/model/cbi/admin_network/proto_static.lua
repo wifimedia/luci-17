9530ea3bd782cf715a208f52d4aa4112002aaf39
DungTD <duntd8x@gmail.com>
2017-12-22 22:43:34 +0700

    fix

--- modules/luci-base/luasrc/model/cbi/admin_network/proto_static.lua
+++ modules/luci-base/luasrc/model/cbi/admin_network/proto_static.lua
@@ -35,7 +35,7 @@ dns = section:taboption("general", DynamicList, "dns",
 dns.datatype = "ipaddr"
 dns.cast     = "string"
 
-
+--[[
 if luci.model.network:has_ipv6() then
 
 	local ip6assign = section:taboption("general", Value, "ip6assign", translate("IPv6 assignment length"),
@@ -74,7 +74,7 @@ if luci.model.network:has_ipv6() then
 
 end
 
-
+]]--
 luci.tools.proto.opt_macaddr(section, ifc, translate("Override MAC address"))
 
 
