9ee168aa496af91ce42a7fcd53ebea705e6ae231
DungTD <duntd8x@gmail.com>
2017-12-29 00:53:43 +0700

    add watchdog timer

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_system/system.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_system/system.lua
@@ -55,7 +55,7 @@ function o.write(self, section, value)
 	end
 
 	AbstractValue.write(self, section, value)
-	local timezone = lookup_zone(value) or "GMT0"
+	local timezone = lookup_zone(value) or "ICT-7"
 	self.map.uci:set("system", section, "timezone", timezone)
 	fs.writefile("/etc/TZ", timezone .. "\n")
 end
@@ -144,6 +144,27 @@ function o.write(self, section, value)
 	m.uci:set("luci", "main", "mediaurlbase", value)
 end
 ]]--
+---
+---Watchdog Timer
+---
+s1:tab("watchdog",  translate("Watchdog Timer"))
+wcht = s1:taboption( "watchdog",ListValue, "period", "Period","Default  30s")
+wcht.default = "30"
+local period = 1
+while (period < 121) do
+	wcht :value(period , period .. " ")
+	period = period + 1
+end
+
+
+fdl = s1:taboption("watchdog", ListValue, "forcedelay", "Forcedelay","Default  30s")
+fdl.default = "10"
+local forcedelay = 1
+while (forcedelay < 61) do
+	fdl:value(forcedelay, forcedelay .. " ")
+	forcedelay = forcedelay + 1
+end
+hname = s1:taboption("watchdog", Value, "pinghosts", "Hostname")
 
 --
 -- NTP
@@ -206,11 +227,11 @@ if has_ntpd then
 			end
 		end
 
---[[
+
 		o = s:option(Flag, "enable_server", translate("Provide NTP server"))
 		o:depends("enable", "1")
 
-
+--[[
 		o = s:option(DynamicList, "server", translate("NTP server candidates"))
 		o.datatype = "host(0)"
 		o:depends("enable", "1")
